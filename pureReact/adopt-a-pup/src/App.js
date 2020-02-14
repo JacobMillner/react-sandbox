801cc11b636e1321dc51ed8ef45e10544049d0c6
Jacob Millner <jacob.millner@outlook.com>
2020-02-14 17:28:34 -0500

    Passing props into pet component.

--- pureReact/adopt-a-pup/src/App.js
+++ pureReact/adopt-a-pup/src/App.js
@@ -1,17 +1,25 @@
-const Pet = () => {
+const Pet = ({name, animal, breed}) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese")
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Ruby Rails",
+      animal: "Cat",
+      breed: "Blond"
+    }),
+    React.createElement(Pet, { name: "Molly", animal: "Cat", breed: "Black" })
   ]);
 };
 
