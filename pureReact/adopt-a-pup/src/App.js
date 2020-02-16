365fb888bab7a706ff2ed63f407fb3bb79b1c2bf
Jacob Millner <jacob.millner@outlook.com>
2020-02-16 16:31:18 -0500

    Added gitignore for npm packages and formatted code.

--- pureReact/adopt-a-pup/src/App.js
+++ pureReact/adopt-a-pup/src/App.js
@@ -1,4 +1,4 @@
-const Pet = ({name, animal, breed}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
