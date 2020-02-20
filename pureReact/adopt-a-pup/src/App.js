459386e5d0793f932037a4770da3f5c6f5a9f522
Jacob Millner <jacob.millner@outlook.com>
2020-02-19 20:42:20 -0500

    Extracted pet component to separate file.

--- pureReact/adopt-a-pup/src/App.js
+++ pureReact/adopt-a-pup/src/App.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import { Pet } from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
