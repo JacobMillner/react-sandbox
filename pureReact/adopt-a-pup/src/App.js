62366044aa9f2cf7dc1b2cf310650fa022ecec63
Jacob Millner <jacob.millner@outlook.com>
2020-02-19 20:36:05 -0500

    Installed/configured parcel and eslint.

--- pureReact/adopt-a-pup/src/App.js
+++ pureReact/adopt-a-pup/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
@@ -23,4 +26,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
