fc8cbbc76cab521a86e16abf8534b09393f6f26f
Jacob Millner <jacob.millner@outlook.com>
2020-05-11 18:49:35 -0400

    adding location hook

--- pureReact/adopt-a-pup/src/SearchParams.js
+++ pureReact/adopt-a-pup/src/SearchParams.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
