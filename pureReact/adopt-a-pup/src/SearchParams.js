c27c2e268cc9b1517a78c206e4818c94da50a3a3
Jacob Millner <jacob.millner@outlook.com>
2020-05-13 17:05:05 -0400

    implementing pet api for options dropdown

--- pureReact/adopt-a-pup/src/SearchParams.js
+++ pureReact/adopt-a-pup/src/SearchParams.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -14,6 +16,20 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={e => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
