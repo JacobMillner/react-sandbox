4e30a1e708a6a632932c63ee07927d89635ad88b
jmillner <jacob.millner@outlook.com>
2022-03-08 18:58:54 -0500

    create search params component with useState hooks

--- pureReact/adopt-a-pup-2/src/App.js
+++ pureReact/adopt-a-pup-2/src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+      <SearchParams />
     </div>
   );
 };
---
+++ pureReact/adopt-a-pup-2/src/SearchParams.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
