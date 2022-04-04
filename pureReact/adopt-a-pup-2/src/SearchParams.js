4bf5fce283b4114b9df2281045e24f68f3c21336
jmillner <jacob.millner@outlook.com>
2022-04-04 18:59:37 -0400

    add animal option dropdown

--- pureReact/adopt-a-pup-2/src/SearchParams.js
+++ pureReact/adopt-a-pup-2/src/SearchParams.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -13,6 +16,21 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
